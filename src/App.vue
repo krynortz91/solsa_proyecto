bf7fce26ecb928a8068ecb7428bb93a0757990ac
Sappy <macarenaramdohr@gmail.com>
2023-06-11 19:34:18 -0400

    se crean secciones, js, html y css

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,26 @@
 <template>
-  <NavBar/>
-  <router-view />
-  <Whatsapp />
-  <TopArrow />
-  <HomeView />
-  <ServiciosView />
-  <GaleriaView />
-  <TestimonioComp/>
-  <ContactoComp/>
-  <FooterComp/>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import NavBar from "@/components/NavBar.vue" 
-import FooterComp from "@/components/FooterComp.vue"
-import ContactoComp from "@/components/ContactoComp.vue"
-import TestimonioComp from "@/components/TestimonioComp.vue"
-import Whatsapp from "@/components/Whatsapp.vue"
-import TopArrow from "@/components/TopArrow.vue"
-import HomeView from "@/views/HomeView.vue"
-import ServiciosView from "@/views/ServiciosView.vue"
-import GaleriaView from "@/views/GaleriaView.vue"
-
-
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    NavBar,
-    FooterComp,
-    ContactoComp,
-    TestimonioComp,
-    Whatsapp,
-    GaleriaView,
-    HomeView,
-    ServiciosView,
-    TopArrow
+    HelloWorld
   }
 }
 </script>
 
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
